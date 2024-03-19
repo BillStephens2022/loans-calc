dde48f7322c1b7c639fc74b66d7189434cc23378
BillStephens2022 <two4onebill@yahoo.com>
2024-03-19 10:07:36 -0400

    added event handlers to form so that form values can be captured in state variables

--- pages/LoanCalcs.js
+++ pages/LoanCalcs.js
@@ -1,31 +1,79 @@
-
+import { useState } from "react";
 
 const LoanCalcs = () => {
+  const [commitment, setCommitment] = useState();
+  const [fundedLoan, setFundedLoan] = useState();
+  const [upfrontFees, setUpfrontFees] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const unfundedCommitment = commitment - fundedLoan;
+    console.log("======= BALANCE SHEET ===========");
+    console.log("CR. Cash", -fundedLoan + upfrontFees);
+    console.log("DR. Loan", fundedLoan);
+    console.log("CR. DEFERRED FEES", -upfrontFees);
+
+    console.log("======= OFF BALANCE SHEET ===========");
+    console.log("unfunded commitment: ", unfundedCommitment);
+  };
+
   return (
     <div>
-    <h1>Loan Journal Entries</h1>
-    <h2>Enter Loan Details and submit to see simulated journal entries</h2>
-    <div>
-      <form>
-        <div>
+      <h1>Loan Journal Entries</h1>
+      <h2>Enter Loan Details and submit to see simulated journal entries</h2>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
             <label htmlFor="commitment">commitment:</label>
-            <input id="commitment" name="commitment" />
-        </div>
-        <div>
+            <input
+              type="text"
+              id="commitment"
+              name="commitment"
+              value={commitment || ""}
+              onChange={(e) => {
+                const value = parseFloat(e.target.value);
+                if (!isNaN(value)) {
+                  setCommitment(value);
+                }
+              }}
+            />
+          </div>
+          <div>
             <label htmlFor="fundedloan">total funded:</label>
-            <input id="fundedloan" name="fundedloan" />
-        </div>
-        <div>
+            <input
+              type="text"
+              id="fundedloan"
+              name="fundedloan"
+              value={fundedLoan || ""}
+              onChange={(e) => {
+                const value = parseFloat(e.target.value);
+                if (!isNaN(value)) {
+                  setFundedLoan(value);
+                }
+              }}
+            />
+          </div>
+          <div>
             <label htmlFor="upfrontfees">upfront fees:</label>
-            <input id="upfrontfees" name="upfrontfees" />
-        </div>
-        <button>Submit</button>
-      </form>
-      
+            <input
+              type="text"
+              id="upfrontfees"
+              name="upfrontfees"
+              value={upfrontFees || ""}
+              onChange={(e) => {
+                const value = parseFloat(e.target.value);
+                if (!isNaN(value)) {
+                  setUpfrontFees(value);
+                }
+              }}
+            />
+          </div>
+          <button>Submit</button>
+        </form>
       </div>
-     
     </div>
   );
-}
+};
 
 export default LoanCalcs;
