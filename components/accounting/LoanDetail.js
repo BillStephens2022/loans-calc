4e455df3b3feac227685b2f64ead121e8d24cc9a
BillStephens2022 <two4onebill@yahoo.com>
2024-03-20 11:45:13 -0400

    moved journal entry table to separate component

--- components/accounting/LoanDetail.js
+++ components/accounting/LoanDetail.js
@@ -1,5 +1,7 @@
+
 import { formatAmount } from "@/util/formatting";
 import { JournalEntry } from "@/util/helperClasses";
+import JournalEntryTable from "@/components/accounting/JournalEntryTable";
 import classes from "@/components/accounting/LoanDetail.module.css";
 
 const LoanDetail = ({
@@ -57,63 +59,8 @@ const LoanDetail = ({
 
       <div className={classes.loanDetail_journalEntries}>
         <h2>Journal Entries</h2>
-        <h3>Balance Sheet</h3>
-
-        <table className={classes.journalEntry_table}>
-          <thead>
-            <tr className={classes.tableRow_header}>
-              <th className={classes.tableCell}>Account</th>
-              <th className={classes.tableCell}>High Level Category</th>
-              <th className={classes.tableCell}>Category</th>
-              <th
-                className={`${classes.tableCell} ${classes.tableCellAmountHeader}`}
-              >
-                Debit
-              </th>
-              <th
-                className={`${classes.tableCell} ${classes.tableCellAmountHeader}`}
-              >
-                Credit
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {journalEntries.map((entry, index) => (
-              <tr key={index} className={classes.tableRow}>
-                <td className={classes.tableCell}>{entry.account}</td>
-                <td className={classes.tableCell}>{entry.highLevelCategory}</td>
-                <td className={classes.tableCell}>{entry.category}</td>
-                <td
-                  className={`${classes.tableCell} ${classes.tableCellAmounts}`}
-                >
-                  {entry.isDebit ? formatAmount(entry.amount) : ""}
-                </td>
-                <td
-                  className={`${classes.tableCell} ${classes.tableCellAmounts}`}
-                >
-                  {!entry.isDebit ? formatAmount(entry.amount) : ""}
-                </td>
-              </tr>
-            ))}
-            <tr className={classes.tableRow}>
-              <td className={classes.tableCell}>
-                <strong>Total Debits/(Credits)</strong>
-              </td>
-              <td className={classes.tableCell}></td>
-              <td className={classes.tableCell}></td>
-              <td
-                className={`${classes.tableCell} ${classes.tableCellAmounts}`}
-              >
-                <strong>{formatAmount(debitTotal)}</strong>
-              </td>
-              <td
-                className={`${classes.tableCell} ${classes.tableCellAmounts}`}
-              >
-                <strong>{formatAmount(creditTotal)}</strong>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <JournalEntryTable journalEntries={journalEntries} debitTotal={debitTotal} creditTotal={creditTotal} />
+       
 
         <h3>Off Balance Sheet</h3>
         <p>Unfunded Loan Commitment: {formatAmount(unfundedCommitment)}</p>
