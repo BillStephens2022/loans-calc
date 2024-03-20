296941c24d0a6ebaebfd4696a8dceebda3c9ca1a
BillStephens2022 <two4onebill@yahoo.com>
2024-03-20 07:10:59 -0400

    updated home page with animated text for the header

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,13 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={classes.home_main}>
-        <h1 className={classes.home_header}>Corporate Loans</h1>
-        <h2 className={classes.home_header}>Accounting & Risk Examples</h2>
+        <div className={classes.home_header_container}>
+        <ul className={classes.home_header_ul}>
+          <li>C</li><li>O</li><li>R</li><li>P</li><li>O</li><li>R</li><li>A</li><li>T</li><li>E</li>
+          <li>&nbsp;</li><li>L</li><li>O</li><li>A</li><li>N</li><li>S</li>
+        </ul>
+        </div>
+        <h2 className={classes.home_subheader}>Accounting & Risk Examples</h2>
       </main>
     </>
   );
