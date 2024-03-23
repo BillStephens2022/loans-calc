ddd421824ffeb2ca7dd0fe8d2295bf48b4772963
BillStephens2022 <two4onebill@yahoo.com>
2024-03-23 13:04:14 -0400

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
-import path from 'path'; // Import path module
+const path = require('path');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname); // Import path and use it here
+module.exports = {
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },
 };
-
-export default nextConfig;
-
-
-
