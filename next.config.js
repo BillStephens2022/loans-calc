11c0a3c20d2a5530ca43f50b5efdb1a38e0e3669
BillStephens2022 <two4onebill@yahoo.com>
2024-03-23 12:58:21 -0400

    update nextConfig

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
-/** @type {import('next').NextConfig} */
-import path, { __dirname } from "path";
+import path from 'path'; // Import path module
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.alias['@'] = path.join(__dirname, './'); // Assuming your root is where the 'pages' directory resides
+    config.resolve.alias['@'] = path.resolve(__dirname); // Import path and use it here
     return config;
   },
-}
+};
 
 export default nextConfig;
 
