81f53b0e04e4f6eb3625b349dc94bee4d45a675f
BillStephens2022 <two4onebill@yahoo.com>
2024-03-23 12:53:20 -0400

    updated config files for deployment

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+/** @type {import('next').NextConfig} */
+const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname); // Assuming your root is where the 'pages' directory resides
+    config.resolve.alias['@'] = path.join(__dirname, './'); // Assuming your root is where the 'pages' directory resides
     return config;
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig;
 
 
 
