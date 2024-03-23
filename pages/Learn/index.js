610e1a923e09bcb68223d24231c263e4d543e196
BillStephens2022 <two4onebill@yahoo.com>
2024-03-23 09:25:46 -0400

    updated styling on topic tabs

--- pages/Learn/index.js
+++ pages/Learn/index.js
@@ -5,28 +5,57 @@ import FacilityTypes from "@/components/learn/FacilityTypes";
 import Utilization from "@/components/learn/Utilization";
 import classes from "@/pages/Learn/Learn.module.css";
 
-
 const Learn = () => {
-    const [topic, setTopic] = useState(<LoanTypes />);
+  const [topic, setTopic] = useState({ type: "loan", content: <LoanTypes /> });
 
-    return (
-        <div>
-            <PageHeader><h1>Learn</h1></PageHeader>
-            <main className={classes.main}>
-                <aside className={classes.aside}>
-              
-                  <ul className={classes.topicsList}>
-                    <li className={classes.topic} onClick={() => setTopic(<LoanTypes />)}>Types of Loans</li>
-                    <li className={classes.topic} onClick={() => setTopic(<FacilityTypes />)}>Facility Types</li>
-                    <li className={classes.topic} onClick={() => setTopic(<Utilization />)}>Utilization</li>
-                  </ul>
-                </aside>
-                <div className={classes.contentContainer}>
-                    {topic}
-                </div>
-            </main>
-        </div>
-    )
-}
+  const handleClick = (content) => {
+    setTopic(content);
+  };
+
+  return (
+    <div>
+      <PageHeader>
+        <h1>Learn</h1>
+      </PageHeader>
+      <main className={classes.main}>
+        <aside className={classes.aside}>
+          <ul className={classes.topicsList}>
+            <li
+              className={`${classes.topic} ${
+                topic.type === "loan" ? classes.active : ""
+              }`}
+              onClick={() =>
+                handleClick({ type: "loan", content: <LoanTypes /> })
+              }
+            >
+              Types of Loans
+            </li>
+            <li
+              className={`${classes.topic} ${
+                topic.type === "facility" ? classes.active : ""
+              }`}
+              onClick={() =>
+                handleClick({ type: "facility", content: <FacilityTypes /> })
+              }
+            >
+              Facility Types
+            </li>
+            <li
+              className={`${classes.topic} ${
+                topic.type === "utilization" ? classes.active : ""
+              }`}
+              onClick={() =>
+                handleClick({ type: "utilization", content: <Utilization /> })
+              }
+            >
+              Utilization
+            </li>
+          </ul>
+        </aside>
+        <div className={classes.contentContainer}>{topic.content}</div>
+      </main>
+    </div>
+  );
+};
 
 export default Learn;
