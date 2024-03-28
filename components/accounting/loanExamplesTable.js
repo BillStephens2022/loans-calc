f0f984d2f431898814faaf1222f3a13b5f23ac88
BillStephens2022 <two4onebill@yahoo.com>
2024-03-28 10:21:38 -0400

    added totals to loan examples table and included weighted average cost and weighted average mark for the portfolio

--- components/accounting/loanExamplesTable.js
+++ components/accounting/loanExamplesTable.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import { GoTrash } from "react-icons/go";
 import { formatAmount } from "../../util/formatting";
 import Button from "../ui/button";
@@ -21,12 +21,53 @@ const LoanExamplesTable = ({ examples, onDelete, showButtons }) => {
   };
 
   const handleRowClick = (exampleId, event) => {
-    if (!event.target.closest('button')) {
+    if (!event.target.closest("button")) {
       router.push(`/accounting/${exampleId}`);
     }
-     
-    
   };
+
+  const calculateUnfundedCommitment = (example) => {
+    return example.commitment - example.fundedLoan - example.lettersOfCredit;
+  };
+  
+  const calculateLoanMTM = (example) => {
+    let loanMTM =
+      (example.commitment * (example.loanMark - example.weightedAverageCost)) / 100;
+    if (
+      (example.accounting === 'HFS' && loanMTM > 0) ||
+      example.accounting === 'HFI'
+    ) {
+      loanMTM = 0;
+    }
+    return loanMTM;
+  };
+
+   // Function to calculate totals for specified columns
+   const calculateColumnTotal = (column) => {
+    return examples.reduce((total, example) => total + example[column], 0);
+  };
+
+  // Calculate column totals
+  const totalCommitment = calculateColumnTotal('commitment');
+  const totalFundedLoans = calculateColumnTotal('fundedLoan');
+  const totalLettersOfCredit = calculateColumnTotal('lettersOfCredit');
+  const totalUnfundedCommitment = examples.reduce((total, example) => {
+    return total + calculateUnfundedCommitment(example);
+  }, 0);
+  const totalUpfrontFees = calculateColumnTotal('upfrontFee');
+  const totalLoanMTM = examples.reduce((total, example) => {
+    return total + calculateLoanMTM(example);
+  }, 0);
+
+  const calculateWeightedAverageMark = () => {
+    const weightedSum = examples.reduce((total, example) => {
+      return total + example.commitment * example.loanMark;
+    }, 0);
+    const totalCommitment = calculateColumnTotal('commitment');
+    return weightedSum / totalCommitment;
+  };
+
+  const weightedAverageMark = calculateWeightedAverageMark();
 
   return (
     <div className={classes.tableWrapper}>
@@ -45,61 +86,62 @@ const LoanExamplesTable = ({ examples, onDelete, showButtons }) => {
               <th>Weighted Average Cost</th>
               <th>Loan Mark</th>
               <th>MTM</th>
-              {/* {showButtons && <th>View Acctg Details</th>} */}
               {showButtons && <th>Delete</th>}
             </tr>
           </thead>
           <tbody>
             {examples.map((example) => {
-              let loanMTM =
-                (example.commitment *
-                  (example.loanMark - example.weightedAverageCost)) /
-                100;
-              if (
-                (example.accounting === "HFS" && loanMTM > 0) ||
-                example.accounting === "HFI"
-              ) {
-                loanMTM = 0;
-              }
-
-          
-
               return (
-                
-                  <tr key={example._id} className={classes.tableBodyRows} onClick={(event) => handleRowClick(example._id, event)}>
-                    <td>{example.borrower}</td>
-                    <td>{example.facility}</td>
-                    <td>{example.accounting}</td>
-                    <td>{formatAmount(example.commitment)}</td>
-                    <td>{formatAmount(example.fundedLoan)}</td>
-                    <td>{formatAmount(example.lettersOfCredit)}</td>
-                    <td>
-                      {formatAmount(
-                        example.commitment -
-                          example.fundedLoan -
-                          example.lettersOfCredit
-                      )}
+                <tr
+                  key={example._id}
+                  className={classes.tableBodyRows}
+                  onClick={(event) => handleRowClick(example._id, event)}
+                >
+                  <td>{example.borrower}</td>
+                  <td>{example.facility}</td>
+                  <td>{example.accounting}</td>
+                  <td>{formatAmount(example.commitment)}</td>
+                  <td>{formatAmount(example.fundedLoan)}</td>
+                  <td>{formatAmount(example.lettersOfCredit)}</td>
+                  <td>{formatAmount(calculateUnfundedCommitment(example))}</td>
+                  <td>{formatAmount(example.upfrontFee)}</td>
+                  <td>{example.weightedAverageCost.toFixed(4)}</td>
+                  <td>{example.loanMark.toFixed(4)}</td>
+                  <td>{formatAmount(calculateLoanMTM(example))}</td>
+                  {showButtons && (
+                    <td className={classes.deleteCell}>
+                      <Button
+                        className="deleteButton"
+                        onClick={(event) =>
+                          handleDeleteExample(example._id, event)
+                        }
+                        disabled={loading}
+                      >
+                        <GoTrash />
+                      </Button>
                     </td>
-                    <td>{formatAmount(example.upfrontFee)}</td>
-                    <td>{example.weightedAverageCost.toFixed(4)}</td>
-                    <td>{example.loanMark.toFixed(4)}</td>
-                    <td>{formatAmount(loanMTM)}</td>
-                    {showButtons && (
-                      <td className={classes.deleteCell}>
-                        <Button
-                          className="deleteButton"
-                          onClick={(event) => handleDeleteExample(example._id, event)}
-                          disabled={loading}
-                        >
-                          <GoTrash />
-                        </Button>
-                      </td>
-                    )}
-                  </tr>
-                
+                  )}
+                </tr>
               );
             })}
+           
           </tbody>
+          <tfoot>
+            <tr className={classes.tableBodyRows}>
+              <td className={classes.totalCell}>Total:</td>
+              <td className={classes.totalCell}></td>
+              <td className={classes.totalCell}></td>
+              <td className={classes.totalCell}>{formatAmount(totalCommitment)}</td>
+              <td className={classes.totalCell}>{formatAmount(totalFundedLoans)}</td>
+              <td className={classes.totalCell}>{formatAmount(totalLettersOfCredit)}</td>
+              <td className={classes.totalCell}>{formatAmount(totalUnfundedCommitment)}</td>
+              <td className={classes.totalCell}>{formatAmount(totalUpfrontFees)}</td>
+              <td className={classes.totalCell}>{((totalCommitment-totalUpfrontFees)/totalCommitment * 100).toFixed(4)}</td>
+              <td className={classes.totalCell}>{weightedAverageMark.toFixed(4)}</td>
+              <td className={classes.totalCell}>{formatAmount(totalLoanMTM)}</td>
+              {showButtons && <td></td>}
+            </tr>
+          </tfoot>
         </table>
       </div>
     </div>
