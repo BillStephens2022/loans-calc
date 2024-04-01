dc6a456c5970bffa781f50bb65ab23b62b17f7df
BillStephens2022 <two4onebill@yahoo.com>
2024-04-01 09:48:05 -0400

    added a quiz page with 10 questions about corp loans

---
+++ pages/quiz/index.js
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import PageHeader from "../../components/layout/pageHeader";
+import quizData from "../../lib/quizData";
+import classes from "./quiz.module.css";
+import Button from "../../components/ui/button";
+
+const Quiz = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [isAnswerSubmitted, setIsAnswerSubmitted] = useState(false);
+  const currentQuestion = quizData.questions[currentQuestionIndex];
+  const totalQuestions = quizData.questions.length;
+
+  const handleNextQuestion = () => {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+    setIsAnswerSubmitted(false); // Reset answer submission status for the next question
+    setSelectedAnswer(null); // Reset selected answer for the next question
+  };
+
+  const handleSubmitAnswer = () => {
+    if (selectedAnswer === currentQuestion.correct_answer) {
+      setScore(score + 1);
+    }
+    setIsAnswerSubmitted(true);
+  };
+
+  // Check if all questions have been answered
+  const isQuizFinished = currentQuestionIndex === totalQuestions;
+
+  const handleRetakeQuiz = () => {
+    setCurrentQuestionIndex(0); // Reset to the first question
+    setScore(0); // Reset the score
+    setSelectedAnswer(null); // Reset the selected answer
+    setIsAnswerSubmitted(false); // Reset the answer submission status
+  };
+
+  return (
+    <main className={classes.quizPage}>
+      <PageHeader>
+        <h1 className={classes.pageHeader}>Quiz</h1>
+      </PageHeader>
+      <main className={classes.main}>
+        {!isQuizFinished ? (
+          <>
+            <h2>{currentQuestion.question}</h2>
+            <form className={classes.quizForm}>
+              {Object.keys(currentQuestion.answers).map((key) => (
+                <div key={key}>
+                  <input
+                    className={classes.radioInput}
+                    type="radio"
+                    id={key}
+                    name="answer"
+                    value={key}
+                    checked={selectedAnswer === key}
+                    onChange={() => setSelectedAnswer(key)}
+                  />
+                  <label htmlFor={key}>{currentQuestion.answers[key]}</label>
+                </div>
+              ))}
+            </form>
+            {isAnswerSubmitted && (
+              <div>
+                <p>
+                  {selectedAnswer === currentQuestion.correct_answer
+                    ? `Correct! ${score} of ${
+                        currentQuestionIndex + 1
+                      } correct.`
+                    : `Incorrect! ${score} of ${
+                        currentQuestionIndex + 1
+                      } correct.`}
+                  <p>{currentQuestion.explanation}</p>
+                </p>
+              </div>
+            )}
+            {!isAnswerSubmitted && (
+              <Button className="m_1" onClick={handleSubmitAnswer}>
+                Submit Answer
+              </Button>
+            )}
+            {isAnswerSubmitted && (
+              <Button className="m_1" onClick={handleNextQuestion}>
+                Next
+              </Button>
+            )}
+          </>
+        ) : (
+          <div>
+            <p>
+              Quiz Finished! Final Score: {score} of {totalQuestions} correct
+            </p>
+            <Button onClick={handleRetakeQuiz}>Retake Quiz</Button>
+          </div>
+        )}
+      </main>
+    </main>
+  );
+};
+
+export default Quiz;
