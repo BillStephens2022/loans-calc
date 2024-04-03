5e652b1b6213424ada3019ae0850e040cf95f8a4
BillStephens2022 <two4onebill@yahoo.com>
2024-04-03 07:32:35 -0400

    updated comments for Accounting Page

--- pages/accounting/index.js
+++ pages/accounting/index.js
@@ -1,31 +1,44 @@
 import { useState, useEffect } from "react";
 import useSWR from "swr";
-import PageHeader from "../../components/layout/pageHeader";
-import LoanAccountingForm from "../../components/accounting/loanAccountingForm";
 import {
   createLoanAccountingExample,
   getLoanAccountingExamples,
   deleteLoanAccountingExampleById,
   getJournalEntries,
 } from "../../lib/api";
-import classes from "./accounting.module.css";
+import PageHeader from "../../components/layout/pageHeader";
+import LoanAccountingForm from "../../components/accounting/loanAccountingForm";
 import LoanExamplesTable from "../../components/accounting/loanExamplesTable";
 import Button from "../../components/ui/button";
 import Modal from "../../components/ui/modal";
 import FullBalanceSheet from "../../components/accounting/fullBalanceSheet";
 import OffBalanceSheetTable from "../../components/accounting/offBalanceSheetTable";
 import BlinkingInstructions from "../../components/ui/blinkingInstructions";
-import AccountingPieChart from "@/components/accounting/accountingPieChart";
+import AccountingPieChart from "../../components/accounting/accountingPieChart";
+import classes from "./accounting.module.css";
 
-// Accounting Page
+// Accounting Page summary - renders table of loan examples (LoanExamplesTable) and buttons where user can opt to
+// add new examples via a modal form, view the full balance sheet of the examples portfolio (FullBalanceSheet),
+// the Off Balance Sheet (OffBalanceSheetTable), or view the total commitments by accounting methodoloy (AccountingPieChart).
+// The LoanExamplesTable is interactive, so user can click an individual loan example's accounting details via
+// dynamic page routing.
 
 const Accounting = ({ loanAccountingExamples }) => {
+  // state for setting examples as data is retrieved from the database as well as new 
+  // examples are created or existing examples are deleted.  Note that getStaticProps is used
+  // for server side rendering for the exising examples in the database.
   const [examples, setExamples] = useState(loanAccountingExamples || {});
+  // state for setting journal entries as data is retrieved from the database.  note that journal
+  // entries are associated with loan examples as well via the models.
   const [journalEntries, setJournalEntries] = useState([]);
+  // state for opening / closing the modal containing the LoanAccountingForm for entering loan examples
   const [isModalOpen, setIsModalOpen] = useState(false);
+  // toggles showing / hiding the FullBalanceSheet component
   const [showBalanceSheet, setShowBalanceSheet] = useState(false);
+  // toggles showing / hiding the accountingPieChart component
   const [showChart, setShowChart] = useState(false);
 
+  // useSWR hook used for data (examples & journal entries) retrieval
   const { data: updatedExamples, error } = useSWR(
     ["/api/accounting/", "/api/entries"], // Include both endpoints
     async () => {
@@ -41,7 +54,10 @@ const Accounting = ({ loanAccountingExamples }) => {
     }
   );
 
+ 
   useEffect(() => {
+    // sort the examples by borrower name (alphabetical order) and update state
+    // so examples are rendered in order
     if (updatedExamples) {
       const sortedExamples = updatedExamples.loanExamples.sort((a, b) =>
         a.borrower.localeCompare(b.borrower)
@@ -51,6 +67,9 @@ const Accounting = ({ loanAccountingExamples }) => {
     }
   }, [updatedExamples]);
 
+  // handle the loanAccountingForm submit (in the pop up modal). Uses
+  // imported custom function 'createAccountingExample' to post the
+  // example to the database and close the modal.
   const handleFormSubmit = async (formData) => {
     try {
       await createLoanAccountingExample(formData);
@@ -61,15 +80,20 @@ const Accounting = ({ loanAccountingExamples }) => {
     }
   };
 
+  // handle deleting an example from the database when a delete button 
+  // is clicked on a specific example in the LoanExamplesTable component.
   const handleDeleteExample = async (exampleId) => {
     try {
+      // imported custom function to delete specific example from the database
       await deleteLoanAccountingExampleById(exampleId);
+      // remove deleted item from examples dataset and reset state
       setExamples(examples.filter((example) => example._id !== exampleId));
     } catch (error) {
       console.error("Error deleting example:", error.message);
     }
   };
 
+  // calculate total unfunded commitments across examples portfolio
   const totalUnfundedCommitments = examples.reduce((total, example) => {
     return (
       total +
@@ -77,6 +101,7 @@ const Accounting = ({ loanAccountingExamples }) => {
     );
   }, 0);
 
+  // calculate total letters of credit across examples portfolio
   const totalLettersOfCredit = examples.reduce((total, example) => {
     return total + example.lettersOfCredit;
   }, 0);
@@ -96,6 +121,7 @@ const Accounting = ({ loanAccountingExamples }) => {
   const totalCommitmentHFS = calculateTotalByAccounting("HFS");
   const totalCommitmentFVO = calculateTotalByAccounting("FVO");
 
+  // handles button click for showing either chart or balance sheet
   const handleShowContent = (chart, balanceSheet) => {
     setShowChart(chart);
     setShowBalanceSheet(balanceSheet);
@@ -108,10 +134,13 @@ const Accounting = ({ loanAccountingExamples }) => {
         <h2 className={classes.subHeader}>Examples</h2>
       </PageHeader>
       <div className={classes.formAndButtonContainer}>
+        {/* Instructions for clicking on a specific example in table to see further detail */}
         <BlinkingInstructions page="accounting" />
+        {/* Button to toggle modal opening of form for entering a new loan example */}
         <Button className="addExample" onClick={() => setIsModalOpen(true)}>
           Add Example
         </Button>
+        {/* If modal is open (via button click above), show the form */}
         {isModalOpen && (
           <Modal
             isOpen={isModalOpen}
@@ -122,6 +151,7 @@ const Accounting = ({ loanAccountingExamples }) => {
         )}
       </div>
       <div className={classes.accountingExamplesTableContainer}>
+        {/* Table showing all entered examples from the database */}
         <LoanExamplesTable
           examples={examples}
           onDelete={handleDeleteExample}
@@ -129,12 +159,14 @@ const Accounting = ({ loanAccountingExamples }) => {
         />
       </div>
       <div className={classes.showButtonsDiv}>
+        {/* Show pie chart of commitments by accounting methodology (i.e. HFI, HFS, FVO) */}
         <Button
           className="showButton"
           onClick={() => handleShowContent(true, false)}
         >
           Show Commitments by Acctg
         </Button>
+        {/* Show full balance sheet and off balance sheet of examples portfolio */}
         <Button
           className="showButton"
           onClick={() => handleShowContent(false, true)}
@@ -142,7 +174,7 @@ const Accounting = ({ loanAccountingExamples }) => {
           Show Balance Sheet
         </Button>
       </div>
-
+      {/* Toggled by button above to show the full balance sheet and off balance sheet table */}
       {showBalanceSheet && (
         <div className={classes.balanceSheetView}>
           <h2 className={classes.chartHeader}>
@@ -159,6 +191,7 @@ const Accounting = ({ loanAccountingExamples }) => {
           />
         </div>
       )}
+       {/* Toggled by button above to show the pie chart of commitments by accounting methodology */}
       {showChart && (
         <div className={classes.pieChartWrapper}>
           <h3 className={classes.chartHeader}>Commitments by Accounting Methodology</h3>
@@ -173,6 +206,7 @@ const Accounting = ({ loanAccountingExamples }) => {
   );
 };
 
+// for serving up table of examples
 export async function getStaticProps() {
   let loanAccountingExamples = [];
 
@@ -189,7 +223,7 @@ export async function getStaticProps() {
     props: {
       loanAccountingExamples,
     },
-    revalidate: 1200, // Re-generate page every 1200 seconds (20 minutes)
+    revalidate: 1200, // Re-generate page every 1200 seconds (20 minutes) using loan examples props from the database
   };
 }
 
