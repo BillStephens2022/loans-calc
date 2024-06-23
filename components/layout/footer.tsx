1f7be0208f0efb96eea227a9242937a78eed31c5
BillStephens2022 <two4onebill@yahoo.com>
2024-06-23 06:28:40 -0400

    updated footer to use dynamic copyright year

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -4,9 +4,10 @@ import classes from "./footer.module.css";
 
 // displays footer with copyright and link to Bill Stephens' portfolio page
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className={classes.footer}>
-      <p className={classes.text}>Copyright © 2024, <Link className={classes.link} href="https://billstephens2022.github.io/my_portfolio/">Bill Stephens</Link>. All Rights Reserved.</p>
+      <p className={classes.text}>Copyright © {currentYear}, <Link className={classes.link} href="https://billstephens2022.github.io/my_portfolio/">Bill Stephens</Link>. All Rights Reserved.</p>
     </div>
   );
 }
